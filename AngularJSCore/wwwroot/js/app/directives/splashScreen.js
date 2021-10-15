e6b289e937096e312ae744cb1a02b1de311e5da8
raedmiranda <lalo_sagitario.95@hotmail.es>
2021-10-15 01:45:22 -0500

    splashscreen ok

--- AngularJSCore/wwwroot/js/app/directives/splashScreen.js
+++ AngularJSCore/wwwroot/js/app/directives/splashScreen.js
@@ -1,20 +1,15 @@
 ﻿app.directive('splashscreen', function () {
     return {
         restrict: 'E',
-        transclude: true,
         scope: {},
-        controller: function ($scope, $timeout, $element) {
+        controller: function ($scope, $timeout) {
             $scope.classhide = "";
-
-            //$scope.splash = true;
             $timeout(function () {
                 $scope.classhide = "hiddde";
-                //$scope.splash = false;
             }, 1000);
             $timeout(function () {
                 angular.element(document.getElementsByClassName("hiddde")).remove();
             }, 3010);
-
         },
         templateUrl: "templates/splashScreen.html",
         replace: true
