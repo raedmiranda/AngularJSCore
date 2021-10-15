7da4f21d540b87df24cf511a36ce8e5b07c515f3
raedmiranda <lalo_sagitario.95@hotmail.es>
2021-10-15 02:29:47 -0500

    navbar ok

---
+++ AngularJSCore/wwwroot/js/app/directives/navbar.js
@@ -0,0 +1,9 @@
+﻿app.directive('navbar', function () {
+    return {
+        restrict: 'E',
+        scope: {},
+        controller: "practice",
+        templateUrl: "templates/navbar.html",
+        replace: true
+    }
+});
